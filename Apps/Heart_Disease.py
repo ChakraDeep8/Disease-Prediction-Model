dde05bc8f3b2822147477a995ab6a6d42a4602e8
Deep <deepchakraborty0810@gmail.com>
2023-07-27 23:16:08 +0530

    Toggle remove

--- Apps/Heart_Disease.py
+++ Apps/Heart_Disease.py
@@ -150,17 +150,7 @@ def app():
         st.write(prediction_proba_RF)
         cmb.plt_RF()
 
-    from streamlit_toggle import st_toggle_switch
-    st.write("## Toggle Switch")
-    st_toggle_switch(
-        label="Enable Setting?",
-        key="switch_1",
-        default_value=False,
-        label_after=False,
-        inactive_color="#D3D3D3",  # optional
-        active_color="#11567f",  # optional
-        track_color="#29B5E8",  # optional
-    )
+
 
     def predict_best_algorithm():
         if cmb.best_model == 'Naive Bayes':
