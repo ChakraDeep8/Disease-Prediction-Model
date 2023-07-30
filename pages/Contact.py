ab2de21a19cc5846f0e26a936c4910d99c49de43
Deep <deepchakraborty0810@gmail.com>
2023-07-30 19:34:30 +0530

    Hopefully last update

--- pages/Contact.py
+++ pages/Contact.py
@@ -1,33 +1,7 @@
-import os
-import secrets
-import streamlit as st
+import streamlit as st  # pip install streamlit
 
-# Trick to preserve the state of your widgets across pages
-for k, v in st.session_state.items():
-    st.session_state[k] = v
+st.header(":mailbox: Get In Touch With Me!")
 
-st.markdown("""
-  <style>
-      ul[class="css-j7qwjs e1fqkh3o7"]{
-        position: relative;
-        padding-top: 2rem;
-        display: flex;
-        justify-content: center;
-        flex-direction: column;
-        align-items: center;
-      }
-      .css-17lntkn {
-        font-weight: bold;
-        font-size: 18px;
-        color: grey;
-      }
-      .css-pkbazv {
-        font-weight: bold;
-        font-size: 18px;
-      }
-  </style>""", unsafe_allow_html=True)
-
-st.header("Contact")
 
 contact_form = """
 <form action="https://formsubmit.co/{}" method="POST">
@@ -37,17 +11,14 @@ contact_form = """
      <textarea name="message" placeholder="Your message here"></textarea>
      <button type="submit">Send</button>
 </form>
-""".format(st.secrets["emil_address"])
+""".format(st.secrets["email"])
 
 st.markdown(contact_form, unsafe_allow_html=True)
 
-
 # Use Local CSS File
 def local_css(file_name):
-    path = os.path.dirname(__file__)
-    file_name = path + "/" + file_name
     with open(file_name) as f:
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
 
-local_css("style/email_style.css")
+local_css("style/style.css")
