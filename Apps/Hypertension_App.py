4c2fa2570bc34b2cb651bae402b7207077cc9a23
Deep <deepchakraborty0810@gmail.com>
2023-08-01 20:03:53 +0530

    Hypertension_App.py update

--- Apps/Hypertension_App.py
+++ Apps/Hypertension_App.py
@@ -15,7 +15,7 @@ def app():
 
     lottie_coding = load_lottiefile("res/Yoga_Padmasana.json")
 
-    st.title("Hypertension Blood Pressure Detector")
+    st.title("Hypertension [Blood Pressure] Detector")
     st.info("This app predicts whether a person have any hypertension blood pressure or not")
 
     st.sidebar.header('User Input Features')
